0a61e2c46e12630f203d1c8d00fea02c4ec6750f
1231920712 <83565776+1231920712@users.noreply.github.com>
2021-10-15 18:30:14 +1100

    Delete Team orcale project directory

--- Team orcale project/python-pdf-extractor/find_diam.py
+++
@@ -1,7 +0,0 @@
-class find_diam(object):
-    def find_diam_index(self,data):
-        indexs_diam =[]
-        for i in data:
-            index = i.find('Diam')
-            indexs_diam.append(index)
-        return indexs_diam
